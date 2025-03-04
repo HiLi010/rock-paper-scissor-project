23840318ec3add231276d91fcbc5885840899a15
HiLi010 <hilidot010@gmail.com>
2025-03-04 14:22:06 +0000

    feat: initialize Rock Paper Scissors project

---
+++ scripts/12-rock-paper-scissors.js
@@ -0,0 +1,150 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
+
+updateScoreElement();
+
+/*
+if (!score) {
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+  };
+}
+*/
+
+/*
+const score = {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
+
+/*取出来这个值
+console.log(JSON.parse(localStorage.getItem('score')));
+将strings重新转化回去到object*/
+
+let isAutoPlaying = false;
+let intervalId;
+
+//让自动玩停止和继续
+function autoPlay() {
+  if (!isAutoPlaying) {
+    intervalId = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000)
+    isAutoPlaying = true;
+  } 
+    else {
+      clearInterval(intervalId);
+      isAutoPlaying = false;
+  }
+}
+
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('scissors');
+});
+
+document.body.addEventListener('keydown', (event) => {
+  console.log(event.key);
+
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+});
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+
+  let result = '';
+
+  if (playerMove === 'scissors') {
+    if (computerMove === 'rock') {
+      result = 'You lose.';
+    } else if (computerMove === 'paper') {
+      result = 'You win.';
+    } else if (computerMove === 'scissors') {
+      result = 'Tie.';
+    }
+
+  } else if (playerMove === 'paper') {
+    if (computerMove === 'rock') {
+      result = 'You win.';
+    } else if (computerMove === 'paper') {
+      result = 'Tie.';
+    } else if (computerMove === 'scissors') {
+      result = 'You lose.';
+    }
+    
+  } else if (playerMove === 'rock') {
+    if (computerMove === 'rock') {
+      result = 'Tie.';
+    } else if (computerMove === 'paper') {
+      result = 'You lose.';
+    } else if (computerMove === 'scissors') {
+      result = 'You win.';
+    }
+  }
+
+  /*Update a score*/
+  if (result === 'You win.') {
+    score.wins += 1;
+  } else if (result === 'You lose.') {
+    score.losses += 1;
+  } else if (result === 'Tie.') {
+    score.ties += 1;
+  }
+
+  /*保存这个值*/
+  /*举例localStorage.setItem('message','hello');*/
+  localStorage.setItem('score', JSON.stringify(score)); /*只支持strings，所以要从object转化到字符串*/
+
+  updateScoreElement();
+
+  document.querySelector('.js-result').innerHTML = result;
+
+  //document.querySelector('.js-moves').innerHTML = `You ${playerMove} - ${computerMove} Computer`;
+  document.querySelector('.js-moves').innerHTML = `You
+    <img src="images/${playerMove}-emoji.png" class="move-icon">
+    <img src="images/${computerMove}-emoji.png" class="move-icon">
+    Computer`;
+}
+
+function updateScoreElement() {
+  document.querySelector('.js-score')
+    .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+
+  let computerMove = ''; /*这里放进function来了，但是选择用return来调用上去*/
+ 
+  //1.Computer randomly selects a move
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = 'rock';
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = 'paper';
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = 'scissors';
+  }
+
+  return computerMove;
+  console.log('after'); /*在return之后，不会再run这条code了*/
+}
